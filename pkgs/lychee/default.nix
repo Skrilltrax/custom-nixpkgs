bae44213722cd5c061dc84acdf577776a0a3e290
Harsh Shandilya <me@msfjarvis.dev>
2021-01-04 19:47:57 +0530

    wtf
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- pkgs/lychee/default.nix
+++ pkgs/lychee/default.nix
@@ -13,8 +13,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "08accjk7lsab8qg2f8nmi8f6h7gncs4b090yv2sca456f6yavnhq";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ openssl ];
+  nativeBuildInputs = [ pkgs.pkgconfig ];
+  buildInputs = [ pkgs.openssl ];
 
   cargoSha256 = "01m5zvcrc3yc8kwnbp59lgc9nj06pxsxx6i3gaqmh9ah587m7rm6";
 
